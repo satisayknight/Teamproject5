6f6f6dc912ab9519caea751ff95c0473c6e205ce
Sarthak Singh <ssingh98@uw.edu>
2022-11-10 14:50:47 -0800

    worked on reservation system

--- src/main/java/hoteReservationSystem/bookingInfo/HotelReservation.java
+++ src/main/java/hoteReservationSystem/bookingInfo/HotelReservation.java
@@ -1,10 +1,41 @@
 package hoteReservationSystem.bookingInfo;
 
+import hotelReservationSystem.guest.Guest;
+import hotelReservationSystem.hotel.room.Room;
+
 import java.util.Date;
 
 public class HotelReservation {
 
     private Date checkInDate;
     private Date checkOutDate;
-    //private Guest guest;
+    private Guest guest;
+    private Room room;
+
+    public HotelReservation(Guest guest, Room room, Date checkInDate, Date checkOutDate) {
+        this.guest = guest;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer: " + this.guest.toString()
+                + "\nRoom: " + this.room.toString()
+                + "\nCheckIn Date: " + this.checkInDate
+                + "\nCheckOut Date: " + this.checkOutDate;
+    }
 }
