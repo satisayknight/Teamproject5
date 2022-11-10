c2733cb1de3f2e26b4f95ac6c68c2a9fd83a8b7b
Sarthak Singh <ssingh98@uw.edu>
2022-11-10 14:03:10 -0800

    updated guests

--- src/main/java/hotelReservationSystem/guest/Guest.java
+++ src/main/java/hotelReservationSystem/guest/Guest.java
@@ -3,11 +3,13 @@ package hotelReservationSystem.guest;
 import hotelReservationSystem.guest.membership.MembershipTypes;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Guest {
     private String guestName;
     private String guestEmail;
     private String guestPhoneNumber;
+    private static final String EMAIL_REGEX_PATTERN = "^(.+)@(.+).(.+)$";
 
     MembershipTypes membershipType = null;
     private List<Guest> guestList;
@@ -17,9 +19,18 @@ public class Guest {
     }
 
     public Guest(String name, String email, String phone) {
+        this.isValidEmail(email);
         this.guestName = name;
         this.guestEmail = email;
         this.guestPhoneNumber = phone;
+
+    }
+    private void isValidEmail(final String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX_PATTERN);
+
+        if(!pattern.matcher(email).matches()) {
+            throw new IllegalArgumentException("Invalid email");
+        }
     }
 
     public Guest(String name, String email, String phone, MembershipTypes type) {
@@ -49,5 +60,11 @@ public class Guest {
 
     public void setGuestPhoneNumber(String guestPhoneNumber) {
         this.guestPhoneNumber = guestPhoneNumber;
+    }
+    @Override
+    public String toString() {
+        return "Guest Name: " + this.guestName
+                + " Email: " + this.guestEmail
+                +"Phone Number: " + this.guestPhoneNumber;
     }
 }
