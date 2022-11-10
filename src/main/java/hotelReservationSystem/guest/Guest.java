cbb858b8a24eec6b1c98e7e0f14ab64692f5a7d8
Joe Savella <Christian.Savella@tlgcohort.com>
2022-11-10 13:40:24 -0800

    guest fields

--- src/main/java/hotelReservationSystem/guest/Guest.java
+++ src/main/java/hotelReservationSystem/guest/Guest.java
@@ -8,6 +8,46 @@ public class Guest {
     private String guestName;
     private String guestEmail;
     private String guestPhoneNumber;
+
+    MembershipTypes membershipType = null;
     private List<Guest> guestList;
 
+    public Guest() {
+
+    }
+
+    public Guest(String name, String email, String phone) {
+        this.guestName = name;
+        this.guestEmail = email;
+        this.guestPhoneNumber = phone;
+    }
+
+    public Guest(String name, String email, String phone, MembershipTypes type) {
+        this(name, email, phone);
+        this.membershipType = type;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getGuestEmail() {
+        return guestEmail;
+    }
+
+    public void setGuestEmail(String guestEmail) {
+        this.guestEmail = guestEmail;
+    }
+
+    public String getGuestPhoneNumber() {
+        return guestPhoneNumber;
+    }
+
+    public void setGuestPhoneNumber(String guestPhoneNumber) {
+        this.guestPhoneNumber = guestPhoneNumber;
+    }
 }
