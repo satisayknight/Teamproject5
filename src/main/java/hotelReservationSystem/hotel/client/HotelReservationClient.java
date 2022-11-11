a08e838c150e573ceee74c8722abb95ac866484b
Joe Savella <Christian.Savella@tlgcohort.com>
2022-11-11 13:44:31 -0800

    hotel reservation client switch updates

--- src/main/java/hotelReservationSystem/hotel/client/HotelReservationClient.java
+++ src/main/java/hotelReservationSystem/hotel/client/HotelReservationClient.java
@@ -13,7 +13,6 @@ public class HotelReservationClient {
 
     public static void main(String[] args) {
         Scanner stdInt = new Scanner(System.in);
-
         Room[] amountOfRoomsRequested;
         int selection = 0;
 
@@ -23,19 +22,48 @@ public class HotelReservationClient {
             System.out.println("Enter your selection: ");
             selection = stdInt.nextInt();
 
-            switch(selection) {
+            switch (selection) {
 
                 case 1:
                     System.out.println("How many rooms would you like to book? ");
                     int rooms = stdInt.nextInt();
                     amountOfRoomsRequested = new Room[rooms];
 
-                    for(int i = 0; i < amountOfRoomsRequested.length; i++) {
+                    for (int i = 0; i < amountOfRoomsRequested.length; i++) {
                         System.out.println("What kind of room would you like: King, Queen, or Suite? ");
-                        RoomTypes roomTypeRequested = RoomTypes.valueOf(stdInt.next().toUpperCase());
+                        RoomTypes.Rooms roomTypesRequested = RoomTypes.Rooms.getByName(stdInt.nextLine().toUpperCase());
                     }
+
+                case 2:
+                    System.out.println("What is your reservation id? ");
+                    int reservationId = stdInt.nextInt();
+                    break;
+                case 3:
+                    System.out.println("What is your reservation id? ");
+                    int resservationId = stdInt.nextInt();
+                    break;
+                case 4:
+                    System.out.println("Which kind of room would you like to view? King, Queen, or Suite? ");
+                    String roomTypeToView = stdInt.nextLine();
+                    if (roomTypeToView.equalsIgnoreCase("King")) {
+
+                    } else if (roomTypeToView.equalsIgnoreCase("Queen")) {
+
+                    } else if (roomTypeToView.equalsIgnoreCase("Suite")) {
+
+                    } else {
+                        System.out.println("Invalid. Please input: King, Queen, or Suite.");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Thank you for using the HotelReservationSystem JSS program.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
             }
         }
+        while (selection != 5);
     }
 
     public static void displayMenu() {
