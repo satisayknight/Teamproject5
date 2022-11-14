16844826013a3931c62c8f797bad62c324c404ed
Sarthak Singh <ssingh98@uw.edu>
2022-11-13 21:24:59 -0800

    worked on price and room class figured some things out

--- src/main/java/hotelReservationSystem/bookingInfo/HotelReservation.java
+++ src/main/java/hotelReservationSystem/bookingInfo/HotelReservation.java
@@ -6,7 +6,6 @@ import hotelReservationSystem.hotel.room.Price;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 import java.util.Scanner;
 
 public class HotelReservation {
