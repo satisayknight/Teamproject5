17f7cb8fa576eea41ead4ecf6f19d90664bc481e
Sarthak Singh <ssingh98@uw.edu>
2022-11-15 12:40:33 -0800

    Made new test for Hotel Reservation

---
+++ src/test/java/hotelReservationSystem/bookingInfo/HotelReservationTest.java
@@ -0,0 +1,48 @@
+package hotelReservationSystem.bookingInfo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelReservationTest extends HotelReservation {
+
+    @Test
+    void testGetDays() {
+    }
+
+    @Test
+    void testSetDays() {
+    }
+
+    @Test
+    void testGetGuest() {
+    }
+
+    @Test
+    void testSetGuest() {
+    }
+
+    @Test
+    void testSetRoom() {
+    }
+
+    @Test
+    void testGetReservationId() {
+    }
+
+    @Test
+    void testSetReservationId() {
+    }
+
+    @Test
+    void testGetRoom() {
+    }
+
+    @Test
+    void testGetTotalCost() {
+    }
+
+    @Test
+    void testSetTotalCost() {
+    }
+}
