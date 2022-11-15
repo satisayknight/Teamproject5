64ef64e917e1f529011816da5ea5e9a650450425
Sarthak Singh <ssingh98@uw.edu>
2022-11-14 21:29:36 -0800

    Trying tests for ICatalog

---
+++ src/test/java/hotelReservationSystem/hotel/room/ICatalogTest.java
@@ -0,0 +1,36 @@
+package hotelReservationSystem.hotel.room;
+
+import hotelReservationSystem.bookingInfo.HotelReservation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ICatalogTest extends HotelReservation {
+
+    @org.junit.jupiter.api.Test
+    void findByRoomNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNextRoomByType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByRoomType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findAvailableRoomByRoomType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findTakenRoomByRoomType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void totalNumberOfRooms() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAll() {
+    }
+}
