71334224f6df376621603fec50672b2ab5f8c96d
Sarthak Singh <ssingh98@uw.edu>
2022-11-14 21:43:00 -0800

    Trying tests for ICatalog

--- src/test/java/hotelReservationSystem/hotel/room/ICatalogTest.java
+++ src/test/java/hotelReservationSystem/hotel/room/ICatalogTest.java
@@ -1,6 +1,7 @@
 package hotelReservationSystem.hotel.room;
 
 import hotelReservationSystem.bookingInfo.HotelReservation;
+import hotelReservationSystem.bookingInfo.InMemoryReservationCatalog;
 import org.junit.Before;
 
 import javax.xml.catalog.Catalog;
@@ -9,38 +10,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ICatalogTest extends HotelReservation {
 
-    private Catalog catalog;
+    private InMemoryReservationCatalog catalog;
 
     @Before
     public void setUp() {
-        catalog = new InMemoryCatalog();
+        catalog = new InMemoryReservationCatalog();
     }
+
+
 
     @org.junit.jupiter.api.Test
     void findByRoomNumber() {
+        HotelReservation item1 = catalog.findById(101);
+        assertEquals(Integer.valueOf(1), item1.getRoom());
     }
 
     @org.junit.jupiter.api.Test
-    void getNextRoomByType() {
+    public void testFindByIdNotExists() {
+        assertNull(catalog.findById(2000));
     }
 
-    @org.junit.jupiter.api.Test
-    void findByRoomType() {
-    }
+  // @org.junit.jupiter.api.Test
+  // void getNextRoomByType() {
+  // }
 
-    @org.junit.jupiter.api.Test
-    void findAvailableRoomByRoomType() {
-    }
+ // @org.junit.jupiter.api.Test
+ // void findByRoomType() {
+ // }
 
-    @org.junit.jupiter.api.Test
-    void findTakenRoomByRoomType() {
-    }
+  // @org.junit.jupiter.api.Test
+  // void findAvailableRoomByRoomType() {
+  // }
 
-    @org.junit.jupiter.api.Test
-    void totalNumberOfRooms() {
-    }
+  // @org.junit.jupiter.api.Test
+  // void findTakenRoomByRoomType() {
+  // }
 
-    @org.junit.jupiter.api.Test
-    void getAll() {
-    }
+  // @org.junit.jupiter.api.Test
+  // void totalNumberOfRooms() {
+  // }
+
+  // @org.junit.jupiter.api.Test
+  // void getAll() {
+  // }
 }
