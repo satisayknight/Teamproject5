eae78532f4f1b937880c0949b706e9bfdff000a3
Sarthak Singh <ssingh98@uw.edu>
2022-11-15 08:26:28 -0800

    Trying tests for ICatalog

--- src/test/java/hotelReservationSystem/hotel/room/RoomCatalogTest.java
+++ src/test/java/hotelReservationSystem/hotel/room/RoomCatalogTest.java
@@ -2,8 +2,13 @@ package hotelReservationSystem.hotel.room;
 
 import hotelReservationSystem.bookingInfo.HotelReservation;
 import hotelReservationSystem.bookingInfo.InMemoryReservationCatalog;
+import hotelReservationSystem.hotel.room.types.RoomTypes;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,8 +35,35 @@ class RoomCatalogTest extends HotelReservation {
     public void testFindByRoomNotExists() {
          assertNull(catalog.findByRoomNumber(String.valueOf(1001)));
       }
-    @Test
+
+
+      @Test
     void findByRoomType() {
+          Collection<Room> items = catalog.findByRoomType(RoomTypes.KING);
+          // sort the results by id (for testing purposes)
+          List<Room> sortedItems = new ArrayList<>(items);
+          //sortedItems.sort(null);  // natural order (id)
+          //System.out.println(sortedItems);
+          assertEquals(20, sortedItems.size());
+          assertEquals("101", sortedItems.get(0).getRoomNumber());
+          assertEquals("102", sortedItems.get(1).getRoomNumber());
+
+          Collection<Room> items2 = catalog.findByRoomType(RoomTypes.QUEEN);
+          List<Room> sortedItems2 = new ArrayList<>(items2);
+          //System.out.println(sortedItems2);
+          assertEquals(20, sortedItems2.size());
+          assertEquals("121", sortedItems2.get(0).getRoomNumber());
+          assertEquals("122", sortedItems2.get(1).getRoomNumber());
+
+          Collection<Room> items3 = catalog.findByRoomType(RoomTypes.SUITE);
+          List<Room> sortedItems3 = new ArrayList<>(items3);
+          //System.out.println(sortedItems2);
+          assertEquals(20, sortedItems3.size());
+          assertEquals("141", sortedItems3.get(0).getRoomNumber());
+          assertEquals("142", sortedItems3.get(1).getRoomNumber());
+
+
+
     }
 
     @Test
