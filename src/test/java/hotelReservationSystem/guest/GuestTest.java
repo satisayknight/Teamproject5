1e3b78ea1fc5223d71c2a0b1e1b8603f1ff0e1fc
Sarthak Singh <ssingh98@uw.edu>
2022-11-17 08:45:36 -0800

    unversioned fill will delete

---
+++ src/test/java/hotelReservationSystem/guest/GuestTest.java
@@ -0,0 +1,61 @@
+package hotelReservationSystem.guest;
+
+import hotelReservationSystem.bookingInfo.HotelReservation;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuestTest extends HotelReservation {
+
+    @Before
+    void setUp(){
+        Guest guest= new Guest("sar","sa@gmail.com","2099999999",true);
+        //guest.setHasBalanceDue(false);
+
+    }
+
+    @Test
+    void setHasBalanceDue() {
+        Guest guest = new Guest();
+        guest.setHasBalanceDue(false);
+        assertEquals(false, guest.getHasBalanceDue());
+
+    }
+
+    @Test
+    void setGuestEmail() {
+    }
+
+    @Test
+    void setGuestPhoneNumber() {
+    }
+
+    @Test
+    void setGuestName() {
+    }
+
+    @Test
+    void setGuestCardNumber() {
+    }
+
+    @Test
+    void getGuestName() {
+    }
+
+    @Test
+    void getGuestEmail() {
+    }
+
+    @Test
+    void getGuestPhoneNumber() {
+    }
+
+    @Test
+    void getHasBalanceDue() {
+    }
+
+    @Test
+    void getGuestCardNumber() {
+    }
+}
