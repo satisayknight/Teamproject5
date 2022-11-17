fe7851283dde6eb284861b0c98c3c09c05e47550
Joe Savella <Christian.Savella@tlgcohort.com>
2022-11-17 13:12:43 -0800

    updated

--- src/main/java/hotelReservationSystem/bookingInfo/InMemoryReservationCatalog.java
+++ src/main/java/hotelReservationSystem/bookingInfo/InMemoryReservationCatalog.java
@@ -72,20 +72,20 @@ public class InMemoryReservationCatalog implements IReservationCatalog {
             LocalDate today = LocalDate.now();
             boolean isAfterToday = false;
 
-            while (!isAfterToday) {
+            while(!isAfterToday){
                 System.out.println("What is the start date for your room reservation? " +
                         "[Correct Format example YYYY-MM-DD]");
-                LocalDate startDate = LocalDate.parse(stdInt.nextLine());
-                if (startDate.isBefore(today)) {
+                LocalDate startDate=LocalDate.parse(stdInt.nextLine());
+                if(startDate.isBefore(today)){
 
                     System.out.println("Please select a date that is not before today.");
-                } else {
-                    isAfterToday = true;
+                }else{
+                    isAfterToday=true;
                     reservation.setStartDate(startDate);
                 }
             }
 
-        } catch (DateTimeParseException e) {
+        }catch (DateTimeParseException e){
             System.out.println("Please input a correct date [Correct Format example YYYY-MM-DD]");
             startDateToBook();
         }
@@ -112,6 +112,7 @@ public class InMemoryReservationCatalog implements IReservationCatalog {
 
     @Override
     public void createRoomForReservation() throws IllegalArgumentException, InputMismatchException {
+
         try {
             //Set roomType and set Room based on user inputs
             System.out.println("Which room would you like to book? 1:King  2:Queen  3:Suite");
